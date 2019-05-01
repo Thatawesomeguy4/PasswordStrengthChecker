3243d5289820fdecc68585bea5dc0973b6106420
Thatawesomeguy4 <derekteepee@gmail.com>
2019-05-01 16:35:51 -0400

    Working towards a functional dictionary load

--- PasswordCheckServer/DictionaryPage.xaml.cs
+++ PasswordCheckServer/DictionaryPage.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,14 +23,72 @@ namespace PasswordCheckServer
     /// </summary>
     public partial class DictionaryPage : Page
     {
+        string dictionaryPath;
+        List<string> dictionary;
+
         public DictionaryPage()
         {
             InitializeComponent();
+            //disable start button, will be re-enabled once the dictionary is loaded.
+            startButton.IsEnabled = false;
+            //set dictionary path
+            //dictionaryPath = "";
+            
+
+            //load the dictionary
+            Thread loader = new Thread(new ThreadStart(() => LoadDictionary()));
+            loader.Start();
         }
 
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void LoadDictionary()
+        {
+            //set up progress bar for loader.
+            this.Dispatcher.Invoke((Action)(() =>
+            {
+                progressBar_Loader.Minimum = 0;
+                progressBar_Loader.Maximum = 100;
+            }));
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = assembly.GetManifestResourceNames().Single(str => str.EndsWith("10-million-password-list-top-1000000.txt")); ;
+
+            //get number of lines in dictionary file
+            using (File dictionary = assembly.)
+            int lineCount = File.ReadLines(dictionaryPath).Count();
+            int currentLine = 0;
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader file = new StreamReader(stream))
+            {
+                string ln;
+                double currentPercent = 0;
+                double previousPercent = 0;
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    dictionary.Add(ln);
+                    currentLine++;
+                    //calculate new percent
+                    currentPercent = Math.Round((double) (currentLine / lineCount));
+                    //only update UI if percentage has meaningfully changed
+                    if (currentPercent > previousPercent)
+                    {
+                        this.Dispatcher.Invoke((Action)(() =>
+                        {
+                            progressBar_Loader.Value = currentPercent;
+                            previousPercent = currentPercent;
+                        }));
+                    }
+                }
+
+                //close the file
+                file.Close();
+            }
         }
     }
 }
